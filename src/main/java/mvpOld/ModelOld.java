a41bb9bb9e094a02a6ab63e219cbbf863e7bc7da
iZnoSK <92318918+iZnoSK@users.noreply.github.com>
2024-04-24 16:40:50 +0200

    0.9.1 - Všetko

---
+++ src/main/java/mvpOld/ModelOld.java
@@ -0,0 +1,290 @@
+package mvpOld;
+
+import dataOld.*;
+
+import filesOld.LoaderOfDataOld;
+import optimalizaciaOld.*;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+public class ModelOld {
+    public static final int REZERVA_1 = 0;
+    public static final int REZERVA_2 = 0;
+    public static final int DEPO = 59;
+    public static final int C_VODIC = 100;
+    public static final int C_KM = 2;
+    public static final int K = 10000;
+    public static final int DT_MAX = 10 * 60;
+    public static final int T_MAX = 13 * 60;
+    private LinkedHashMap<DvojicaOld<Integer, Integer>, UsekOld> useky;
+    private LinkedHashMap<DvojicaOld<Integer, Integer>, Integer> DT;
+    private LinkedHashMap<DvojicaOld<Integer, Integer>, Integer> T;
+    private LinkedHashMap<Integer, SpojOld> spoje;
+    private LinkedHashMap<Integer, ZastavkaOld> zastavky;
+    private LinkedHashMap<Integer, LinkaOld> linky;
+
+    public ModelOld()
+    {
+        this.useky = new LinkedHashMap<>();
+        this.spoje = new LinkedHashMap<>();
+        this.DT = new LinkedHashMap<>();
+        this.T = new LinkedHashMap<>();
+        this.zastavky = new LinkedHashMap<>();
+        this.linky = new LinkedHashMap<>();
+    }
+
+    public String nacitajData(String pSuborUseky, String pSuborSpoje)
+    {
+        LoaderOfDataOld loader = new LoaderOfDataOld();
+        try
+        {
+            loader.nacitajUseky(pSuborUseky, this.useky, this.zastavky);
+        }
+        catch (Exception e)
+        {
+            return "Chyba pri načítavaní úsekov!";
+        }
+
+        try
+        {
+            loader.nacitajSpoje(pSuborSpoje, this.spoje, this.linky, this.zastavky);
+        }
+        catch (Exception e)
+        {
+            return "Chyba pri načítavaní spojov!";
+        }
+
+        for (SpojOld spoj_i : this.spoje.values())
+        {
+            int miestoPrichodu_i = spoj_i.getMiestoPrichoduID();
+            int casPrichodu_i = spoj_i.getCasPrichoduMinuty();
+            for (SpojOld spoj_j : this.spoje.values())
+            {
+                int miestoOdchodu_j = spoj_j.getMiestoOdchoduID();
+                int casOdchodu_j = spoj_j.getCasOdchoduMinuty();
+                int casPrejazdu = this.useky.get(new DvojicaOld<>(miestoPrichodu_i, miestoOdchodu_j)).getCasPrejazdu();
+
+                if (casPrichodu_i + casPrejazdu + REZERVA_1 <= casOdchodu_j)
+                {
+                    //System.out.println(spoj_i.getID() + " : " + spoj_j.getID());
+                    int casMedziSpojmi = spoj_j.getCasOdchoduMinuty() - spoj_i.getCasPrichoduMinuty();
+                    int casJazdy = (casMedziSpojmi - casPrejazdu < 10) ? casMedziSpojmi : casPrejazdu;
+                    this.DT.put(new DvojicaOld<>(spoj_i.getID(), spoj_j.getID()), casJazdy);
+                    this.T.put(new DvojicaOld<>(spoj_i.getID(), spoj_j.getID()), casMedziSpojmi);
+
+                    //System.out.println("cas prejazdu: " + casPrejazdu);
+                    //System.out.println("DT: " + casJazdy);
+                    //System.out.println("T:" + casMedziSpojmi);
+                    //System.out.println();
+                    spoj_i.pridajNaslednostSpoja(spoj_j);
+                }
+
+                int dist1 = this.useky.get(new DvojicaOld<>(miestoPrichodu_i, DEPO)).getCasPrejazdu();
+                int dist2 = this.useky.get(new DvojicaOld<>(DEPO, miestoOdchodu_j)).getCasPrejazdu();
+                if (casPrichodu_i + dist1 + dist2 + REZERVA_2 <= casOdchodu_j)
+                {
+                    spoj_i.pridajNaslednostSpojaVodic(spoj_j);
+                }
+            }
+        }
+        return "Načítanie údajov bolo úspešné.";
+    }
+
+    //TODO prerobiť - bez vypisov o časoch prejazdov
+    public String[][] vypisVsetkySpoje()
+    {
+        String[][] udajeSpoje = new String[this.spoje.size()][10];
+        int counter = 0;
+        for (SpojOld spoj_i: this.spoje.values())
+        {
+            udajeSpoje[counter] = spoj_i.vypis(this.useky);
+            counter++;
+        }
+        return udajeSpoje;
+    }
+
+    //TODO prerobiť - bez vypisov o časoch prejazdov
+    public LinkedHashMap<Integer, String[][]> vypisVsetkyLinky()
+    {
+        LinkedHashMap<Integer, String[][]> linkyUdaje = new LinkedHashMap<>();
+        for (LinkaOld linka_i: this.linky.values())
+        {
+            linkyUdaje.put(linka_i.getID(), linka_i.vypis(this.useky));
+        }
+
+        return linkyUdaje;
+    }
+
+    public String vykonajMinimalizaciuAutobusov(ArrayList<String[]> pTurnusyUdaje, ArrayList<String[][]> pSpojeUdaje)
+    {
+        try
+        {
+            MinimalizaciaAutobusovOld minBusov = new MinimalizaciaAutobusovOld(this.spoje);
+            int pocetBusov = minBusov.getPocetAutobusov();
+            ArrayList<TurnusOld> turnusy = minBusov.vytvorTurnusy(this.spoje);
+            for (TurnusOld turnus: turnusy)
+            {
+                pTurnusyUdaje.add(turnus.getPrvaZmena().vypis(this.useky));
+                pSpojeUdaje.add(turnus.getPrvaZmena().vypisSpoje(this.useky));
+
+                if(turnus.getDruhaZmena() != null)
+                {
+                    pTurnusyUdaje.add(turnus.getDruhaZmena().vypis(this.useky));
+                    pSpojeUdaje.add(turnus.getDruhaZmena().vypisSpoje(this.useky));
+                }
+            }
+            return String.valueOf(pocetBusov);
+        }
+        catch (Exception e)
+        {
+            return "Chyba pri načítavaní úsekov!";
+        }
+    }
+
+    public String vykonajMinimalizaciuPrazdnychPrejazdov(int pPocetBusov,
+                                                         ArrayList<String[]> pTurnusyUdaje,
+                                                         ArrayList<String[][]> pSpojeUdaje)
+    {
+        try
+        {
+            MinimalizaciaPrejazdovOld minBusov = new MinimalizaciaPrejazdovOld(pPocetBusov, this.spoje, this.useky);
+            boolean bezChyby = false;
+            while(!bezChyby)
+            {
+                bezChyby = minBusov.vytvorSkontrolujTurnusy(this.spoje, this.useky, this.DT, this.T);
+            }
+
+            int prazdnePrejazdy = minBusov.getPrazdnePrejazdy();
+            ArrayList<TurnusOld> turnusy = minBusov.getTurnusy();
+            for (TurnusOld turnus: turnusy)
+            {
+                pTurnusyUdaje.add(turnus.getPrvaZmena().vypis(this.useky));
+                pSpojeUdaje.add(turnus.getPrvaZmena().vypisSpoje(this.useky));
+
+                if(turnus.getDruhaZmena() != null)
+                {
+                    pTurnusyUdaje.add(turnus.getDruhaZmena().vypis(this.useky));
+                    pSpojeUdaje.add(turnus.getDruhaZmena().vypisSpoje(this.useky));
+                }
+            }
+            return String.valueOf(prazdnePrejazdy);
+        }
+        catch (Exception e)
+        {
+            return "Chyba pri načítavaní úsekov!";
+        }
+    }
+
+    public String vykonajMinimalizaciuVodicov(int pPocetBusov,
+                                              ArrayList<String[]> pTurnusyUdaje, ArrayList<String[][]> pSpojeUdaje)
+    {
+        try
+        {
+            MinimalizaciaVodicovOld minVodicov = new MinimalizaciaVodicovOld(pPocetBusov, this.spoje, this.useky, this.DT, this.T);
+
+            boolean bezChyby = false;
+            int count = 2;
+            while(!bezChyby)
+            {
+                System.out.println("----------------------------" + count + ". Iterácia ------------------------------------------");
+                bezChyby = minVodicov.vytvorSkontrolujTurnusy(this.spoje, this.useky, this.DT, this.T);
+                count++;
+            }
+            minVodicov.vypisPremenne();
+            int pocetVodicov = minVodicov.getPocetVodicov();
+            ArrayList<TurnusOld> turnusy = minVodicov.getTurnusy();
+            for (TurnusOld turnus: turnusy)
+            {
+                pTurnusyUdaje.add(turnus.getPrvaZmena().vypis(this.useky));
+                pSpojeUdaje.add(turnus.getPrvaZmena().vypisSpoje(this.useky));
+
+                if(turnus.getDruhaZmena() != null)
+                {
+                    pTurnusyUdaje.add(turnus.getDruhaZmena().vypis(this.useky));
+                    pSpojeUdaje.add(turnus.getDruhaZmena().vypisSpoje(this.useky));
+                }
+            }
+            return String.valueOf(pocetVodicov);
+        }
+        catch (Exception e)
+        {
+            return "Chyba pri načítavaní úsekov!";
+        }
+    }
+
+    public String vykonajMaximalizaciuObsadenosti(int pPocetBusov,
+                                                  int pPocetVodicov,
+                                                  ArrayList<String[]> pTurnusyUdaje, ArrayList<String[][]> pSpojeUdaje)
+    {
+        try
+        {
+            MaximalizaciaObsadenostiOld maxObs = new MaximalizaciaObsadenostiOld(pPocetBusov, pPocetVodicov,this.linky, this.spoje, this.useky, this.DT, this.T);
+
+            boolean bezChyby = false;
+            int count = 2;
+            while(!bezChyby)
+            {
+                System.out.println("----------------------------" + count + ". Iterácia ------------------------------------------");
+                bezChyby = maxObs.vytvorSkontrolujTurnusy(this.spoje, this.useky, this.DT, this.T);
+                count++;
+            }
+            maxObs.vypisPremenne();
+            ArrayList<TurnusOld> turnusy = maxObs.getTurnusy();
+            for (TurnusOld turnus: turnusy)
+            {
+                pTurnusyUdaje.add(turnus.getPrvaZmena().vypis(this.useky));
+                pSpojeUdaje.add(turnus.getPrvaZmena().vypisSpoje(this.useky));
+
+                if(turnus.getDruhaZmena() != null)
+                {
+                    pTurnusyUdaje.add(turnus.getDruhaZmena().vypis(this.useky));
+                    pSpojeUdaje.add(turnus.getDruhaZmena().vypisSpoje(this.useky));
+                }
+            }
+            return "";
+        }
+        catch (Exception e)
+        {
+            return "Chyba pri načítavaní úsekov!";
+        }
+    }
+
+    public String vykonajMaximalizaciuObsluzenychSpojov(double y, int pPocetBusov, int pPocetVodicov,
+                                                        ArrayList<String[]> pTurnusyUdaje, ArrayList<String[][]> pSpojeUdaje)
+    {
+        try
+        {
+            MaximalizaciaObsluzenychSpojovOld maxObs = new MaximalizaciaObsluzenychSpojovOld(y, pPocetBusov, pPocetVodicov,
+                    this.linky, this.spoje, this.useky, this.DT, this.T);
+
+            boolean bezChyby = false;
+            int count = 2;
+            while(!bezChyby)
+            {
+                System.out.println("----------------------------" + count + ". Iterácia ------------------------------------------");
+                bezChyby = maxObs.vytvorSkontrolujTurnusy(this.spoje, this.useky, this.DT, this.T);
+                count++;
+            }
+            maxObs.vypisPremenne();
+            ArrayList<TurnusOld> turnusy = maxObs.getTurnusy();
+            System.out.println(maxObs.pocetObsSpojov());
+            for (TurnusOld turnus: turnusy)
+            {
+                pTurnusyUdaje.add(turnus.getPrvaZmena().vypis(this.useky));
+                pSpojeUdaje.add(turnus.getPrvaZmena().vypisSpoje(this.useky));
+
+                if(turnus.getDruhaZmena() != null)
+                {
+                    pTurnusyUdaje.add(turnus.getDruhaZmena().vypis(this.useky));
+                    pSpojeUdaje.add(turnus.getDruhaZmena().vypisSpoje(this.useky));
+                }
+            }
+            return "";
+        }
+        catch (Exception e)
+        {
+            return "Chyba pri načítavaní úsekov!";
+        }
+    }
+}
