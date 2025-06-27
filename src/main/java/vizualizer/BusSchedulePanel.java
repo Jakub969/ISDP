62ffe969ce3e51c0ac969963db72ec8719814b5a
patri <patrikfilicko@gmail.com>
2025-06-27 14:01:07 +0200

    v 0.1

--- src/main/java/vizualizer/BusSchedulePanel.java
+++ src/main/java/vizualizer/BusSchedulePanel.java
@@ -2,6 +2,8 @@ package vizualizer;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.List;
@@ -9,11 +11,13 @@ import java.util.List;
 public class BusSchedulePanel extends JPanel {
 
     private final Map<String, List<Ride>> busLines = new LinkedHashMap<>();
+    private final List<SegmentInfo> clickableSegments = new ArrayList<>();
+
 
     public BusSchedulePanel() {
         busLines.put("Autobus 1", Arrays.asList(
-                new Ride("04:00", "04:05", "Zastavka 1", "Zastavka 2", true),
-                new Ride("04:25", "04:30", "Zastavka 2", "Zastavka 3", false), // nebol zastavka, len prejazd
+                new Ride("04:00", "04:14", "Zastavka 1", "Zastavka 2", true),
+                new Ride("04:15", "04:30", "Zastavka 2", "Zastavka 3", false), // nebol zastavka, len prejazd
                 new Ride("04:50", "04:55", "Zastavka 3", "Zastavka 4", true)
         ));
 
@@ -29,6 +33,19 @@ public class BusSchedulePanel extends JPanel {
                 new Ride("04:50", "04:55", "Zastavka 3", "Zastavka 4", true)
         ));
 
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                Point p = e.getPoint();
+                for (SegmentInfo si : clickableSegments) {
+                    if (si.area.contains(p)) {
+                        JOptionPane.showMessageDialog(BusSchedulePanel.this, si.info, "Info", JOptionPane.INFORMATION_MESSAGE);
+                        break;
+                    }
+                }
+            }
+        });
+
     }
 
     @Override
@@ -39,17 +56,19 @@ public class BusSchedulePanel extends JPanel {
         int marginLeft = 100;
         int marginTop = 50;
         int lineSpacing = 80;
-        int lineLength = 1700;
+        int lineLength = 2300;
 
         long timeStart = toMillis("04:00");
-        long timeEnd = toMillis("15:30");
+        long timeEnd = toMillis("20:00");
         long timeRange = timeEnd - timeStart;
 
         int busIndex = 0;
 
+        clickableSegments.clear();
+
         for (Map.Entry<String, List<Ride>> entry : busLines.entrySet()) {
             String busName = entry.getKey();
-            List<Ride> stops = entry.getValue();
+            List<Ride> rides = entry.getValue();
 
             int y = marginTop + busIndex * lineSpacing + 15;
 
@@ -59,34 +78,9 @@ public class BusSchedulePanel extends JPanel {
             g2.drawLine(marginLeft, y, marginLeft + lineLength, y);
             g2.drawString(busName, 10, y + 5);
 
-            for (Ride stop : stops) {
-                long arrival = toMillis(stop.start);
-                long departure = toMillis(stop.end);
-
-                double arrivalRatio = (double) (arrival - timeStart) / timeRange;
-                double departureRatio = (double) (departure - timeStart) / timeRange;
-
-                int xArrival = marginLeft + (int) (arrivalRatio * lineLength);
-                int xDeparture = marginLeft + (int) (departureRatio * lineLength);
-
-                // Šedá čiara medzi príchodom a odchodom
-                g2.setColor(Color.LIGHT_GRAY);
-                g2.fillRect(xArrival, y - 3, xDeparture - xArrival, 6);
-
-                // Bodky
-                g2.setColor(Color.BLUE);
-                g2.fillOval(xArrival - 4, y - 4, 8, 8);
-                g2.setColor(Color.RED);
-                g2.fillOval(xDeparture - 4, y - 4, 8, 8);
-
-                // Popis zastávky
-                g2.setColor(Color.DARK_GRAY);
-                g2.drawString(stop.startName + " (" + stop.start + "–" + stop.end + ")", xDeparture + 5, y - 10);
-            }
-
-            for (int i = 0; i < stops.size() - 1; i++) {
-                Ride current = stops.get(i);
-                Ride next = stops.get(i + 1);
+            for (int i = 0; i < rides.size() - 1; i++) {
+                Ride current = rides.get(i);
+                Ride next = rides.get(i + 1);
 
                 long depTime = toMillis(current.end);
                 long arrTime = toMillis(next.start);
@@ -97,15 +91,81 @@ public class BusSchedulePanel extends JPanel {
                 int xStart = marginLeft + (int) (depRatio * lineLength);
                 int xEnd = marginLeft + (int) (arrRatio * lineLength);
 
-                // Farba podľa toho, či zastavil na ďalšej zastávke
-                if (next.deadheadTrip) {
+                g2.setColor(Color.LIGHT_GRAY);
+                g2.setStroke(new BasicStroke(4)); // hrubšia čiara prejazdu
+                g2.drawLine(xStart, y, xEnd, y);
+
+                long pauseMinutes = (arrTime - depTime) / (60 * 1000);
+                String waitInfo = "Čakanie medzi jazdami: " + pauseMinutes + " min ("
+                        + formatTime(depTime) + " – " + formatTime(arrTime) + ")";
+
+                clickableSegments.add(new SegmentInfo(
+                        new Rectangle(xStart, y - 2, xEnd - xStart, 4),
+                        waitInfo
+                ));
+
+            }
+
+            boolean drawUp = true;
+            for (Ride ride : rides) {
+                long arrival = toMillis(ride.start);
+                long departure = toMillis(ride.end);
+
+                double arrivalRatio = (double) (arrival - timeStart) / timeRange;
+                double departureRatio = (double) (departure - timeStart) / timeRange;
+
+                int xArrival = marginLeft + (int) (arrivalRatio * lineLength);
+                int xDeparture = marginLeft + (int) (departureRatio * lineLength);
+
+                if (ride.deadheadTrip) {
                     g2.setColor(Color.GREEN);
                 } else {
                     g2.setColor(Color.YELLOW);
                 }
+                g2.fillRect(xArrival, y - 3, xDeparture - xArrival, 6);
 
-                g2.setStroke(new BasicStroke(4)); // hrubšia čiara prejazdu
-                g2.drawLine(xStart, y, xEnd, y);
+                String rideInfo = "Autobus " + busName + ": " +
+                        ride.startName + " → " + ride.endName +
+                        " (" + ride.start + " – " + ride.end + ")" +
+                        (ride.deadheadTrip ? " [BEŽNÁ JAZDA]" : " [PREJAZD]");
+
+                clickableSegments.add(new SegmentInfo(
+                        new Rectangle(xArrival, y - 3, xDeparture - xArrival, 6),
+                        rideInfo
+                ));
+
+
+
+
+                // Bodky
+                g2.setColor(Color.BLUE);
+                g2.fillOval(xArrival - 4, y - 4, 8, 8);
+                g2.setColor(Color.RED);
+                g2.fillOval(xDeparture - 4, y - 4, 8, 8);
+
+                // Popis zastávky
+                g2.setColor(Color.DARK_GRAY);
+                if (drawUp) {
+                    g2.drawString(ride.startName, xArrival -10, y - 10);
+                    drawUp = !drawUp;
+                }
+
+                else {
+                    g2.drawString(ride.startName, xArrival -10, y + 20);
+                    drawUp = !drawUp;
+                }
+
+                if (ride.equals(rides.get(rides.size() - 1))) {
+                    if (drawUp) {
+                        g2.drawString(ride.endName, xDeparture -10, y - 10);
+                        drawUp = !drawUp;
+                    }
+
+                    else {
+                        g2.drawString(ride.endName, xDeparture -10, y + 20);
+                        drawUp = !drawUp;
+                    }
+                }
             }
 
             busIndex++;
@@ -114,12 +174,16 @@ public class BusSchedulePanel extends JPanel {
 
         // Časová os
         g2.setColor(Color.BLACK);
-        for (int i = 0; i <= 1000; i += 15) {
+        int i = 0;
+        while (true) {
             long t = timeStart + i * 60 * 1000;
+            if (t > timeEnd) {
+                break;
+            }
             double ratio = (double) (t - timeStart) / timeRange;
             int x = marginLeft + (int) (ratio * lineLength);
-            //g2.drawLine(x, marginTop - 10, x, marginTop + busLines.size() * lineSpacing);
             g2.drawString(formatTime(t), x - 15, marginTop - 15);
+            i += 15;
         }
     }
 
